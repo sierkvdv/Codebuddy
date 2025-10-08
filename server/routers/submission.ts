ee205114634f26243f4cbf4d4d7387ab9a7ab60b
sierkvdv <sierkvdv@users.noreply.github.com>
2025-10-08 17:49:40 +0200

    Add missing publicProcedure import to submission router

--- server/routers/submission.ts
+++ server/routers/submission.ts
@@ -1,6 +1,6 @@
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 import {
   getChallengeById,
   createSubmission,
